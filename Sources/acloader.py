9a6a714532c53e216723d683a4e5d471a8659160
Logan Smyth <loganfsmyth@gmail.com>
2010-11-21 18:34:20 -0500

    Working AC file loader

--- Sources/acloader.py
+++ Sources/acloader.py
@@ -1,5 +1,7 @@
 
-import sys
+import sys, pprint
+
+pp = pprint.PrettyPrinter(indent=4)
 
 class ACFormatError(Exception): pass
 
@@ -9,32 +11,110 @@ class ACLoader:
     self.objects = []
     self.file = file(name)
 
-    if not self.file.next().strip() == "AC3Db":
-      raise ACFormatError("File Header does not Match")
+    if not self.file.readline().strip() == "AC3Db":
+      raise ACFormatError("File is not an AC3D file")
 
     for line in self.file:
-      parts = line.strip().split()
-      if parts[0] == 'MATERIAL':
+      line = line.strip()
+      if not self.__parseMaterial(line):
+        obj = self.__parseObject(line)
+        if obj:
+          self.objects.append(obj)
+        else:
+          raise ACFormatError("Error: Expecting material or object")
 
-        if not (parts[2] == 'rgb' and parts[6] == 'amb' and parts[10] == 'emis' and 
-                parts[14] == 'spec' and parts[18] == 'shi' and parts[20] == 'trans'):
-          raise ACFormatError("Material missing lighting components")
+  def __parseMaterial(self, line):
+    if line.startswith('MATERIAL'):
+      parts = line.split()
 
-        self.materials.append({
-          'name' : parts[1].strip('"'),
-          'rgb'  : tuple([float(x) for x in parts[3:6]]),
-          'amb'  : tuple([float(x) for x in parts[7:10]]),
-          'emis' : tuple([float(x) for x in parts[11:14]]),
-          'spec' : tuple([float(x) for x in parts[15:18]]),
-          'shi'  : int(parts[19]),
-          'trans': float(parts[21]),
-        });
+      if not (parts[2] == 'rgb' and parts[6] == 'amb' and parts[10] == 'emis' and 
+              parts[14] == 'spec' and parts[18] == 'shi' and parts[20] == 'trans'):
+        raise ACFormatError("Material missing lighting components")
 
-      elif type == 'OBJECT':
-        obj = {
-          'name':parts[1].stip(),
-        }
+      self.materials.append({
+        'name' : parts[1].strip('"'),
+        'rgb'  : tuple([float(x) for x in parts[3:6]]),
+        'amb'  : tuple([float(x) for x in parts[7:10]]),
+        'emis' : tuple([float(x) for x in parts[11:14]]),
+        'spec' : tuple([float(x) for x in parts[15:18]]),
+        'shi'  : int(parts[19]),
+        'trans': float(parts[21]),
+      });
+      return True
 
+  def __parseObject(self, line):
+    if line.startswith('OBJECT'):
+      obj = { 'type': line.split()[1], 'loc': (0.0, 0.0, 0.0) }
+      for line in self.file:
+        line = line.strip()
+        if line.startswith('name'):
+          obj['name'] = line.split(' ', 1)[1].strip('"')
+        elif line.startswith('data'):
+          num = int(line.split(' ', 1)[1])
+          obj['data'] = self.file.read(num)
+        elif line.startswith('texture'):
+          obj['texture'] = line.split(' ', 1)[1]
+        elif line.startswith('texrep'):
+          obj['texrep'] = tuple([float(x) for x in line.split()[1:3]])
+        elif line.startswith('rot'):
+          nums = [float(x) for x in line.split()[1:]]
+          obj['rot'] = (tuple(nums[0:3]), tuple(nums[3:6]), tuple(nums[6:9]))
+        elif line.startswith('loc'):
+          obj['loc'] = tuple([float(x) for x in line.split()[1:4]])
+        elif line.startswith('url'):
+          obj['url'] = line.split(' ', 1)[1]
+        elif line.startswith('numvert'):
+          obj['verts'] = self.__parseVerts(int(line.split(' ', 1)[1]))
+        elif line.startswith('numsurf'):
+          obj['surfaces'] = self.__parseSurfaces(int(line.split()[1]))
+        elif line.startswith('kids'):
+          obj['kids'] = self.__parseObjects(int(line.split(' ', 1)[1]))
+          break
+
+      return obj
+
+  def __parseSurface(self):
+    header = self.file.next()
+    if not header.startswith('SURF'):
+      raise ACFormatError("Missing surface header")
+
+    surf = { 'type': int(header.split()[1], 16) }
+
+    line = self.file.next()
+    if line.startswith('mat'):
+      surf['mat'] = int(line.split()[1])
+      surf['material'] = self.materials[surf['mat']]
+      line = self.file.next()
+    if line.startswith('refs'):
+      refs = []
+      for i in range(int(line.split()[1])):
+        nums = self.file.next().split()
+        refs.append((int(nums[0]), float(nums[1]), float(nums[2])))
+      surf['refs'] = refs
+    else:
+      raise ACFormatError('Missing surface refs')
+
+    return surf
+
+  def __parseVerts(self, num):
+    verts = []
+    for i in range(num):
+      verts.append(tuple([float(x) for x in self.file.next().strip().split()]))
+
+    return verts
+
+  def __parseSurfaces(self, num):
+    surfs = []
+    for line in range(num):
+      surfs.append(self.__parseSurface())
+
+    return surfs
+
+  def __parseObjects(self, num):
+    objs = []
+    for i in range(num):
+      objs.append(self.__parseObject(self.file.next()))
+    return objs
 
 if __name__ == "__main__":
   if (len(sys.argv) != 2):
@@ -44,3 +124,7 @@ if __name__ == "__main__":
   tmp = ACLoader(sys.argv[1])
 
   print tmp.materials
+  print "-----";
+#  print tmp.objects
+
+  pp.pprint(tmp.objects)
